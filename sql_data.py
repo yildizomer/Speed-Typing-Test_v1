81f985a755efe5b08e3c894209728bbf94a41a29
yildizomer <omerfarukyildiz129@gmail.com>
2023-11-11 18:14:50 +0300

    Speed typing test

--- sql_data.py
+++ sql_data.py
@@ -1,22 +1,26 @@
 # Sql datasında istenilmeyen verilen silinmesi
-
+import requests
 import sqlite3
+
+
+api_url = "https://baconipsum.com/api/?type=meat-and-filler&paras=5&format=text"
+response = requests.get(api_url)
+data = response.text
+
+
+cleaned_data = ''.join(e for e in data if e.isalnum() or e.isspace())  # Sadece harf, rakam ve boşlukları tut
 
 
 conn = sqlite3.connect('kelimeler.db')
 cursor = conn.cursor()
+words = cleaned_data.split()
 
+cursor.execute('''CREATE TABLE IF NOT EXISTS kelimeler (
+                  id INTEGER PRIMARY KEY,
+                  kelime TEXT)''')
 
-silinecek_harfler = '.,-'  
-
-
-sorgu = f"UPDATE kelimeler SET kelime = REPLACE(kelime, '{silinecek_harfler}', '')"
-
-
-cursor.execute(sorgu)
-
-
+for word in words:
+    cursor.execute("INSERT INTO kelimeler (kelime) VALUES (?)", (word,))
 conn.commit()
-
 
 conn.close()
